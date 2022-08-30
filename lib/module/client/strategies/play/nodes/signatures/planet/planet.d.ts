8137957b4bdaf5df95e5504a16fba7e4ea78831e
Mikhail Zamulov <zamulov.m@gor-tel.ru>
2022-08-30 13:25:07 +0300

    update

--- lib/module/client/strategies/play/nodes/signatures/planet/planet.d.ts
+++ lib/module/client/strategies/play/nodes/signatures/planet/planet.d.ts
@@ -1,8 +1,14 @@
 import { NodeBase } from '../../base/base';
 import { T22Equipment } from '../../../../../../../openlib';
 export declare class Planet extends NodeBase {
-    make(type: T22Equipment, level: number): Promise<string>;
-    sell(uuid: string, price: number): Promise<string>;
-    buy(expected: T22Equipment, price: number, count?: number): Promise<string>;
+    make(type: T22Equipment, level: number): Promise<{
+        uuid: string;
+    }>;
+    sell(uuid: string, price: number): Promise<{
+        uuid: string;
+    }>;
+    buy(expected: T22Equipment, price: number, count?: number): Promise<{
+        uuid: string;
+    }>;
     close(uuid: string): Promise<any>;
 }
