65ac4a9843a0eaf0baf6e7cc3d287d49e6ceaefa
Mikhail Zamulov <zamulov.m@gor-tel.ru>
2022-09-02 12:44:35 +0300

    update

--- lib/openlib/settings/interfaces/signatures/core/entity/entity.d.ts
+++ lib/openlib/settings/interfaces/signatures/core/entity/entity.d.ts
@@ -1,16 +1,10 @@
-import { IBShip } from "./signatures/nodes/ship/ship";
-import { IBCargo } from "./signatures/nodes/cargo/cargo";
-import { IBAsteroid } from "./signatures/nodes/asteroid/asteroid";
-import { IBPlanet } from "./signatures/nodes/dockable/signatures/planet/planet";
-import { IBStationBusiness } from "./signatures/nodes/dockable/signatures/station/signatures/52-business";
-import { IBStationScientific } from "./signatures/nodes/dockable/signatures/station/signatures/53-scientific";
 export interface IEntityModel {
     uuid: string;
     owner: string;
     type: number;
     view: number;
     parent: string;
-    body: IBShip | IBPlanet | IBAsteroid | IBCargo | IBStationBusiness | IBStationScientific;
+    body: any;
     nodes?: Array<IEntityModel>;
     metadata?: any;
 }
